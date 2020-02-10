bda852654561c04815022b49ffa6d57c8d17cc3e
jnhuang29 <60870388+jnhuang29@users.noreply.github.com>
2020-02-10 15:25:08 +0800

    hi talk to computer

---
+++ hi.py
@@ -0,0 +1,12 @@
+Python 3.7.5 (tags/v3.7.5:5c02a39a0b, Oct 15 2019, 00:11:34) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("I"+"'m","Eric")
+I'm Eric
+>>> input("name")
+name
+''
+>>> name=input("name")
+nameJacqueline
+>>> print("hi!",name)
+hi! Jacqueline
+>>> 
