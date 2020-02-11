0ebd0e74365577189519789194f9a0ade3cb1a8f
jnhuang29 <60870388+jnhuang29@users.noreply.github.com>
2020-02-11 13:15:14 +0800

    Add files via upload
    Circle

---
+++ Circumference_Area0210.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 10 22:21:59 2020
+
+@author: USER
+"""
+
+#print("Hello World")
+r = input("Enter the radius(cm) of a circle : ")
+P = input("Enter PI : ")
+C = int(r) * 2 * float(P)
+A = int(r) * int(r) * float(P)
+print("Circumfrence is " + (C) , "cm")
+print("Area is " + str(A) , "cm^2" ) 
